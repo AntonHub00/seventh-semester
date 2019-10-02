aa888bdb23d810a8d82f0fd58a89cc4dc68e8251
Antonio Ochoa <antonhub00@gmail.com>
2019-10-02 06:21:06 -0500

    Symbols table "finished" and table output formated in c_minus_semantic in  c_minus_project in automatons

---
+++ automatons/c_minus_project/c_minus_semantic/Id.java
@@ -0,0 +1,20 @@
+public class Id{
+
+    public String name; //lexem
+    public String type; //int, real, void
+    public String category; //var, array, function
+    public int size; //array size or or amount of parameters
+
+    public Id(String name, String type, String category, int size){
+        this.name = name;
+        this.type = type;
+        this.category = category;
+        this.size = size;
+    }
+
+    @Override
+    public String toString(){
+        // return "Id: " + name + "\tType: " + type + "\tCategory: " + category + "\tSize: " + size;
+        return String.format("%20s%20s%20s%20s", name, type, category, size);
+    }
+}
