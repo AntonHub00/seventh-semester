fe855485af925ddf9dbd1980a38a3ee2c8d90f8b
Antonio Ochoa <antonhub00@gmail.com>
2019-11-08 12:00:00 -0600

    Begin of api consumption practice
    Requests already set and working

---
+++ advanced_web_programming/unit_3/practice/vue_project_practice/api_consumption/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <main-view />
+    <button v-on:click="do_request">Click me to get recommendations</button>
+    <li v-for="(the_var, index) in a_var" v-bind:key="index" >{{the_var.Recommendation}}</li>
+  </div>
+</template>
+
+<script>
+import MainView from "./components/MainView.vue";
+
+export default {
+  name: "app",
+  components: {
+    MainView
+  },
+  data() {
+    return {
+      a_var: []
+    };
+  },
+  methods: {
+    do_request() {
+      // const axios = require("axios");
+      console.log("hey");
+
+      this.$axios
+        // .get(this.$BaseUrl+'all', {
+        .get(this.$BaseUrl + "all", {
+          headers: {
+            Authorization:
+              "Bearer 689b0a159879332335d84b6ff4a61a0e6c1619fd8022c39e95ad5d9d3f81"
+          },
+          params: {
+            conditions: "dm"
+          }
+        })
+        .then(response => {
+          this.a_var = response.data.data;
+          console.log(this.a_var);
+        })
+        .catch(error => {
+          this.a_var = error;
+          console.log(this.a_var);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ advanced_web_programming/unit_3/practice/vue_project_practice/api_consumption/src/components/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Hello there</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainView"
+};
+</script>
+
+<style scoped>
+</style>
